cc6e652185035c9988cb4986cc69c214c7c84a83
RishaSalla <salam.kedan@gmail.com>
2026-01-16 02:46:50 +0100

    Update entry.jsx

--- entry.jsx
+++ entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './src/App.jsx'      // <-- أضفنا src هنا
+import './src/index.css'             // <-- أضفنا src هنا
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
