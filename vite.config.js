a48638ddc50def0171c801e16b76756bf21a1018
RishaSalla <salam.kedan@gmail.com>
2026-01-16 02:09:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mafia-Game/',  // <--- هذا السطر هو الأهم! بدونه الشاشة ستكون بيضاء
   server: {
-    host: true // هذا السطر مهم جداً لكي تستطيع فتح اللعبة من الجوال
+    host: true
   }
 })
